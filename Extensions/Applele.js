0e242993bc980c5be9496be9ea6545f20f96635c
dimix <299950422+Dimixixixiixix@users.noreply.github.com>
2026-07-05 03:46:05 -0700

    Update Applele.js

--- Extensions/Applele.js
+++ Extensions/Applele.js
@@ -1,11 +1,12 @@
 class Extension {
-getInfo() {
-  return {
-    id: "applele",
+  getInfo() {
+    return {
+      id: "applele",
     name: "Applele Utilities!",
     color1: "#c92014",
     color2: "#14c94e",
     color3: "#14c94e",
-  };
-}
+      // fun fact:i love apples
+    };
+  }
 }
