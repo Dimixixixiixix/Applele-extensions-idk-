949f4e210c89417e810000327d718ae84b94305b
dimix <299950422+Dimixixixiixix@users.noreply.github.com>
2026-07-07 15:46:58 -0700

    Update Applele.js

--- Extensions/Applele.js
+++ Extensions/Applele.js
@@ -1,14 +1,27 @@
-(function(Scratch) { 
+(function(Scratch) {
   'use strict';
   const blocksIcon = "https://cdn.phototourl.com/free/2026-07-05-db18e682-8ef5-4827-aee3-0030f7b5dad0.png";
+
   class Extension {
     constructor() {
-      this.appol = "false";
+      this.spriteData = new Map();
     }
+
+    getState(target) {
+      if (!this.spriteData.has(target)) {
+        this.spriteData.set(target, false);
+      }
+      return this.spriteData.get(target);
+    }
+
+    setState(target, value) {
+      this.spriteData.set(target, value);
+    }
+
     getInfo() {
       return {
         id: "applele",
-        name: "Applele Blocks",
+        name: "Applele Blocks V2",
         color1: "#ff0000",
         color2: "#00ff00",
         color3: "#00ff00",
@@ -49,20 +62,25 @@
         ]
       };
     }
-    apple() {
-      if (this.appol === "true") {
+
+    apple(args, util) {
+      if (this.getState(util.target) === true) {
         alert("Applele");
       }
     }
-    report() {
-      return this.appol;
+
+    report(args, util) {
+      return this.getState(util.target);
     }
-    stt(args) {
-      this.appol = args.APP === true ? "true" : "false";
+
+    stt(args, util) {
+      this.setState(util.target, args.APP === true);
     }
-    when(args) {
-      return (this.appol === "true") === args.YES;
+
+    when(args, util) {
+      return this.getState(util.target) === args.YES;
     }
   }
+
   Scratch.extensions.register(new Extension());
 })(Scratch);
